8636e29202b1c0d99c0efaab7a7f4cd9a3519857
amirashrafizham <59201954+amirashrafizham@users.noreply.github.com>
2021-06-03 22:19:45 +0800

    Initial Commit

---
+++ test/SimpleAPI.Test/UnitTest1.cs
@@ -0,0 +1,18 @@
+using System;
+using Xunit;
+using SimpleAPI.Controllers;
+
+namespace SimpleAPI.Test
+{
+    public class UnitTest1
+    {
+        WeatherForecastController weatherForecast = new WeatherForecastController();
+
+        [Fact]
+        public void GetReturnsMyName()
+        {
+            var returnValue = weatherForecast.Get(1);
+            Assert.Equal("Amir Ashraf", returnValue.Value);
+        }
+    }
+}
